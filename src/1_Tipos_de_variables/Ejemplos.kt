b58486ddf43b0da4d3bf9001e7e409f2792a7b3e
matiassb15 <matsalasbo15@gmail.com>
2025-03-24 11:06:22 -0600

    comid de ejemplos

---
+++ src/1_Tipos_de_variables/Ejemplos.kt
@@ -0,0 +1,28 @@
+package `1_Tipos_de_variables`
+
+fun main() {
+    //Variable inmutable de tipo entero
+    val edad: Int
+    edad = 48
+    //variable inmutable de tipo  decimal
+    val sueldo: Float
+    sueldo = 1200.55f
+    //Variable inmutable de tipo  decimal
+    val total: Double
+    total = 70000.24
+    //variable inmutable de tipo alfanumerico
+    val titulo = "Sistema de ventas"
+    // Variable mutable, las variables mutables empiezan con "var"
+    var mes: Int
+    mes = 1
+    //algunas lineas mas de codigo
+    mes = 2
+    //variable de tipo Char, almacena un unico caracter
+    var continua: Char = 's'
+    //Variable de tipo Booleano, true false
+    var fin: Boolean = false
+    print("matias")
+    print("alonso")
+    println("salas")
+    println("Bonilla")
+}
---
+++ src/1_Tipos_de_variables/proyecto2.kt
@@ -0,0 +1,27 @@
+package `1_Tipos_de_variables`
+
+fun main(){
+    val variable1: Int
+    variable1 = 7
+    val variable2 = 5
+    var suma: Int
+    suma=12
+    print("el resultado de")
+    print(variable1)
+    print("y")
+    print(variable2)
+    print("es")
+    print(suma)
+    val variable3: Int
+    variable3=12
+    val variable4: Int
+    variable4=12
+    var multiplicacion: Int
+    var = variable3+variable4
+    print("el resultado es")
+    print(variable3)
+    print("y")
+    print(variable4)
+    print("es")
+    print(multiplicacion)
+}
